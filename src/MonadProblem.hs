570b3798f350c0c78089d2cc14a9de46bccceffe
silverice <slifeleaf@gmail.com>
2015-02-14 00:57:52 +0300

    nothing

--- src/MonadProblem.hs
+++ src/MonadProblem.hs
@@ -3,6 +3,7 @@ module MonadProblem where
 -- Based on http://blog.sigfpe.com/2006/08/you-could-have-invented-monads-and.html
 
 import Data.Complex
+import System.Random
 
 -----------------------------------
 f, g :: Float -> Float
@@ -22,6 +23,15 @@ unit v = (v, "")
 bind :: (Float -> (Float, String)) -> ((Float, String) -> (Float, String))
 bind f' (gv, gs) = let (fv, fs) = f' gv in (fv, gs ++ " THEN " ++ fs)
 
+
+--bind :: (a -> b) -> transform -> (b -> b)
+--bind f b = transform b
+
+--(;;) :: (a -> r) -> (a -> r) -> (a -> r)
+--f ;; g = 
+--(bind f' . g' $ 3)
+
+
 -----------------------------------------------------------------------
 -- k E [0, n)
 nthComplexRoot :: (RealFloat a) => Complex a -> Int -> Int -> Complex a
@@ -38,11 +48,26 @@ sqrt', cbrt' :: (RealFloat a) => Complex a -> [Complex a]
 sqrt' c = complexRoots c 2
 cbrt' c = complexRoots c 3
 
+sqBind :: (a -> [a]) -> ([a] -> [a])
+sqBind f [] = []
+sqBind f (h:t) = (f h) ++ (sqBind f t)
 
+sqUnit :: a -> [a]
+sqUnit a = [a]
+
+
+-----------------------
+
+--ex7_bind :: (a → StdGen → (b,StdGen)) → (StdGen → (a,StdGen)) → (StdGen → (b,StdGen))
+--ex7_bind 
+
+------------------------------------------------------------------
 runTest :: (Show testResult) => String -> testResult -> String
 runTest descr testResult = "test '" ++ descr ++ "':\t" ++ (show testResult) ++ "\n"
 
 test :: String
 test = "Testing begins\n"
-		++ runTest "g, f conveyer" (bind f' $ g' 3)
-		++ runTest "complex num" (cbrt' (1 :+ 0))
+		++ runTest "g, . f conveyer" (bind f' . g' $ 3)
+		++ runTest "cubic sq from 1" (cbrt' (1 :+ 0))
+		++ runTest "sqrt' . cbrt' conveyer" (sqBind cbrt' . sqrt' $ (1 :+ 0))
+		++ runTest "random" ((fst . random . mkStdGen $ 1) :: Int)
