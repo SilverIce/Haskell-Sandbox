7b1087c4556d730d54c359623b39915d82dbd36d
fpuser2006881 <slifeleaf@gmail.com>
2015-05-26 01:08:19 -0700

    finally modifiable (in term of Haskell), interactive program that performs actions over binary tree

--- src/BTreeTest.hs
+++ src/BTreeTest.hs
@@ -3,36 +3,36 @@ module BTreeTest (
 )
 where
 
-import qualified Data.List as List
---import qualified Data.Maybe as Maybe
+import qualified Data.List       as List
+import qualified Data.Maybe      as Maybe
 
-import qualified BTree as BT
+import qualified BTree           as BT
 
 --import qualified Control.Monad as CM
-import Data.List.Split (splitWhen)
+import           Data.List.Split (splitWhen)
 
-maybeApply :: (a -> b) -> Maybe a -> Maybe b
-maybeApply f (Just a) = Just $ f a
-maybeApply f Nothing  = Nothing
-
-maybeApply2 :: a -> Maybe (a -> b) -> Maybe b
-maybeApply2 a (Just f) = Just $ f a
-maybeApply2 _ Nothing = Nothing
 
 type BTreeCommand a = [String] -> BT.BTree a -> BT.BTree a
 
 getCommand :: (Ord a, Read a) => String -> Maybe (BT.BTree a -> BT.BTree a)
 getCommand command = do
         let (com:args) = splitWhen (' ' ==) command
-        handler <- getHandler com
-        return (handler args)
+        handlerFunc <- fmap function (getHandler com)
+        return $ handlerFunc args
 
-getHandler :: (Ord a, Read a) => String -> Maybe (BTreeCommand a)
-getHandler command = do
-    (_,handler) <- List.find (\(s,_) -> s == command) handlers
-    return handler
+getHandler :: (Ord a, Read a) => String -> Maybe (Handler a)
+getHandler command = List.find ((== command) . name) handlers
 
-handlers :: (Ord a, Read a) => [(String, BTreeCommand a)]
+data Handler a = Handler { name     :: String
+                         , function :: BTreeCommand a
+                         , comment  :: Maybe String
+                         }
+
+showCommands :: [Handler Int] -> String
+showCommands handlers = foldr (\h s -> s ++ " " ++ name h ++ ": " ++ (Maybe.fromMaybe "" $ comment h) ++ "\n") "" handlers
+
+
+handlers :: (Ord a, Read a) => [Handler a]
 handlers = [
               pair "fromList" (const . BT.fromList . get0)
             , pair "insert" (BT.insert . get0)
@@ -40,25 +40,40 @@ handlers = [
             , pair "merge" (BT.merge . BT.fromList . get0)
             , pair "rebalance" (const BT.rebalance)
             ]
-            where pair a b = (a, b)
+            where pair a b = Handler { name=a, function=b, comment=Just "no comment yet" }
                   get0 (a:_) = read a
 
 
+
+cycle' :: Monad m => (a -> m a) -> a -> m b
+cycle' f x = f x >>= cycle' f
+
 test :: IO ()
 test = do
-    let ls = [1,-9,3,8,2,5,100]
-        tree = BT.fromList ls
-    putStrLn $ "given BTree " ++ show tree
-    putStrLn $ "available commands: " ++ foldr (\(p,_) s -> s ++ " " ++ p) "" (handlers :: [(String, BTreeCommand Int)])
+    --let ls = [1,-9,3,8,2,5,100]
+    --    tree = BT.fromList ls
+    --putStrLn $ "given BTree " ++ show tree
+
+    putStrLn "Hello, let's construct BTree! Input any array of numbers, like [1,9,4]"
+    ntree <- fmap (BT.fromList . (read :: String -> [Int])) getLine
+    putStrLn $ "new tree: " ++ show ntree
+    putStrLn $ "available commands: " ++ showCommands handlers
+
+    cycle' (\t -> do
+        command <- getLine
+        putStrLn $ "your input is: " ++ command
+        let resultingTree = fmap ($ t) (getCommand command)
+        putStrLn $ "result is: " ++ show resultingTree
+        return t) ntree
     --args <- getArgs
     --putStrLn $ show args
     --putStrLn "Hello, let's construct BTree! Input any array of numbers, like [1,9,4]"
     --CM.forever $ do
-    command <- getLine
-    putStrLn $ "your input is: " ++ command
-    let resultingTree = maybeApply2 tree (getCommand command)
-    --let resultingTree = maybeApply2 tree (getCommand2 command)
-    putStrLn $ "result is: " ++ show resultingTree
+    --command <- getLine
+    --putStrLn $ "your input is: " ++ command
+    --let resultingTree = fmap ($ tree) (getCommand command)
+    ----let resultingTree = maybeApply2 tree (getCommand2 command)
+    --putStrLn $ "result is: " ++ show resultingTree
 
 
 
